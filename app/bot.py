3332ae1f8eb2dc88f7e891de7b0a81e7424df0bc
Nikita Zemskov <zemskov271@gmail.com>
2024-04-19 17:39:37 +0500

    feat: add bot logic

--- app/bot.py
+++ app/bot.py
@@ -1,18 +1,49 @@
+import dataclasses
+import json
+
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.enums import ParseMode
+from aiogram.types import URLInputFile
+from nats.aio.msg import Msg
 
 from app.config import settings
+from app.nats import NatsMessageProcessor
+
+
+@dataclasses.dataclass
+class DetectedCarWithPlate:
+    image_url: str
+    plate_number: str
 
 
 root_dispatcher = Dispatcher()
+bot = Bot(settings.TELEGRAM_TOKEN)
+
+MESSAGE = r'''
+**Новое авто**
+*Гос\. Номер*: `{0}`
+'''
 
 
-@root_dispatcher.message(CommandStart)
-async def command_start_handler(message: Message) -> None:
-    await message.answer('я работаю')
+async def process_nats_messages(message: Msg) -> None:
+    data = message.data.decode()
+    payload = DetectedCarWithPlate(**json.loads(data))
+    file = URLInputFile(payload.image_url)
+
+    await bot.send_photo(
+        settings.TELEGRAM_USER_ID,
+        photo=file,
+        caption=MESSAGE.format(payload.plate_number),
+        parse_mode=ParseMode.MARKDOWN_V2,
+    )
 
 
 async def run_bot():
-    bot = Bot(settings.TELEGRAM_TOKEN)
+    nats_processor = NatsMessageProcessor(
+        nats_url=settings.NATS_URL,
+        nats_subject=settings.NATS_SUBJECT,
+        callback=process_nats_messages,
+    )
+    await nats_processor.run()
     await root_dispatcher.start_polling(bot)
+    await nats_processor.stop()
--- app/config.py
+++ app/config.py
@@ -1,12 +1,15 @@
 import logging
 import sys
 
-from pydantic import ConfigDict
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = ConfigDict(extra='ignore')  # Игнорирование лишних переменных в env файле
+    model_config = SettingsConfigDict(
+        extra='ignore',  # Игнорирование лишних переменных в env файле
+        env_file='.env',
+        env_file_encoding='utf-8',
+    )
 
     # Debug
     DEBUG: bool = False
@@ -14,11 +17,14 @@ class Settings(BaseSettings):
 
     # Application
     TELEGRAM_TOKEN: str
+    TELEGRAM_USER_ID: int
 
     # Nats
+    NATS_URL: str
+    NATS_SUBJECT: str
 
 
-settings = Settings(_env_file='.env')
+settings = Settings()
 
 logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler(sys.stdout))
